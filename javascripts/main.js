d72afe5642f410a5a21b7b884b31ffcc09151441
Roger Mbiama Assogo <rogermbiamaassogo@ymail.com>
2012-08-26 18:32:57 +0300

    <script  type="text/javascript" src="http://angosso.net/rvsincludefile/rvsheadpage.js"></script>   <script type="text/javascript" src="http://angosso.net/rvsincludefile/rvsnavigator.js"></script> <script type="text/javascript" src="http://angosso.net/js/publishNavigator/layersmenu-library.js"></script> <script type="text/javascript" src="http://angosso.net/js/publishNavigator/layersmenu.js"></script>  <script  type="text/javascript" src="http://angosso.net/rvsincludefile/rvscustomopenwindow.js"></script>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
 console.log('This would be the main JS file.');
+
+<script  type="text/javascript" src="http://angosso.net/rvsincludefile/rvsheadpage.js"></script>
+
+
+<script type="text/javascript" src="http://angosso.net/rvsincludefile/rvsnavigator.js"></script>
+<script type="text/javascript" src="http://angosso.net/js/publishNavigator/layersmenu-library.js"></script>
+<script type="text/javascript" src="http://angosso.net/js/publishNavigator/layersmenu.js"></script>
+
+<script  type="text/javascript" src="http://angosso.net/rvsincludefile/rvscustomopenwindow.js"></script>
